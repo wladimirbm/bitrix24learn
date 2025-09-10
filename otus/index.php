856095a228c0de487340eaa3f82e2fa1b00d22bd
wladimirbm-local <wladimirbm@gmail.com>
2025-09-11 01:17:26 +0300

    home1-2

--- otus/index.php
+++ otus/index.php
@@ -3,5 +3,5 @@
     $APPLICATION->SetTitle("Список Домашних работ");
 ?>
 <H1><?$APPLICATION->ShowTitle()?></H1>
-<h2><a href="/homework1/">Домашняя работа 1 (Создание и настройка проекта в VScode // ДЗ )</a></h2>
+<h2><a href="homework1/">Домашняя работа 1 (Создание и настройка проекта в VScode // ДЗ )</a></h2>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
