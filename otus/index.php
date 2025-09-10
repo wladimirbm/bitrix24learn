93c1f4a99aad64a92cd125c21c7b0de9659c1157
wladimirbm-local <wladimirbm@gmail.com>
2025-09-11 01:21:11 +0300

    home1-2

--- otus/index.php
+++ otus/index.php
@@ -4,5 +4,5 @@
 ?>
 <H1><?$APPLICATION->ShowTitle()?></H1>
 <h2><a href="homework1/">Домашняя работа 1 (Создание и настройка проекта в VScode // ДЗ)</a></h2>
-<h2><a href="homework1/">Домашняя работа 2 (Отладка и логирование // ДЗ)</a></h2>
+<h2><a href="homework2/">Домашняя работа 2 (Отладка и логирование // ДЗ)</a></h2>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
