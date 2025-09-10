e4bce4838ecd111fe7c60ee3cdcdba592d3b2739
wladimirbm-local <wladimirbm@gmail.com>
2025-09-11 01:23:32 +0300

    home1-2

---
+++ otus/homework2/index.php
@@ -0,0 +1,9 @@
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");?>
+<?php 
+    $APPLICATION->SetTitle("Домашняя работа 1");
+?>
+<H1><?$APPLICATION->ShowTitle()?></H1>
+<?php
+    print_r($_SERVER);
+?>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
