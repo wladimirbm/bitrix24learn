63ed571e95aa5b901a48bf421bcc010a51cf20f1
wladimirbm-local <wladimirbm@gmail.com>
2025-09-11 17:03:45 +0300

    home1-1

--- otus/homework2/index.php
+++ otus/homework2/index.php
@@ -4,6 +4,9 @@
 ?>
 <H1><?$APPLICATION->ShowTitle()?></H1>
 <?php
+    dump($_SERVER);
+    echo "<hr><pre>";
     print_r($_SERVER);
+    echo "</pre>";
 ?>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
