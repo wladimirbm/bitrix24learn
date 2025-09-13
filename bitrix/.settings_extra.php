00e16fc42884627e5ffffb724c0e5ea1f9c36baf
wladimirbm-local <wladimirbm@gmail.com>
2025-09-14 02:55:59 +0300

    gitignore

---
+++ bitrix/.settings_extra.php
@@ -0,0 +1,20 @@
+<?php
+
+return array (
+      'exception_handling' => 
+  array (
+    'value' => 
+    array (
+      'debug' => false,
+      'handled_errors_types' => 4437,
+      'exception_errors_types' => 4437,
+      'ignore_silence' => false,
+      'assertion_throws_exception' => true,
+      'assertion_error_type' => 256,
+      'log' => NULL,
+    ),
+    'readonly' => false,
+  ),
+);
+
+?>
