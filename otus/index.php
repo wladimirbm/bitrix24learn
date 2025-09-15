ae540ed8c6fa0137c72a6da47bf4199cfb02aa72
wladimirbm-local <wladimirbm@gmail.com>
2025-09-15 07:51:32 +0300

    home2

--- otus/index.php
+++ otus/index.php
@@ -1,9 +1,18 @@
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");?>
-<?php 
-    $APPLICATION->SetTitle("Список Домашних работ");
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php"); ?>
+<?php
+$APPLICATION->SetTitle("Список Домашних работ");
 ?>
-<H1><?$APPLICATION->ShowTitle()?></H1>
+<style>
+    .done {
+        background-image: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIiIGhlaWdodD0iMzMiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48Y2lyY2xlIGZpbGw9IiMyMDYxREYiIGN4PSIxNiIgY3k9IjE2LjIiIHI9IjE2Ii8+PHBhdGggZD0iTTE0LjU0IDIyLjA2YTEuMiAxLjIgMCAwMS0uOTUtLjQ4bC0zLjA3LTQuMDRhMS4yIDEuMiAwIDExMS45LTEuNDVsMi4wMiAyLjY2IDUuMDctOC4xM2ExLjIgMS4yIDAgMDEyLjAzIDEuMjZsLTUuOTkgOS42MWMtLjIuMzQtLjU2LjU1LS45Ni41NmgtLjA1eiIgZmlsbC1ydWxlPSJub256ZXJvIiBmaWxsPSIjRkZGIi8+PC9nPjwvc3ZnPg==);
+        height: 16px;
+        width: 16px;
+    }
+</style>
+<H1><? $APPLICATION->ShowTitle() ?></H1>
 <p>Репозиторий: <a href="https://github.com/wladimirbm/bitrix24learn">https://github.com/wladimirbm/bitrix24learn</a></p>
-<h2><a href="homework1/">Домашняя работа 1 (Создание и настройка проекта в VScode // ДЗ)</a></h2>
+<h2><a href="homework1/">Домашняя работа 1 (Создание и настройка проекта в VScode // ДЗ)</a>
+    <div class="done"></div>
+</h2>
 <h2><a href="homework2/">Домашняя работа 2 (Отладка и логирование // ДЗ)</a></h2>
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
