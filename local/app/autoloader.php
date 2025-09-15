d741d55b49b490da72cef42308de06488e0a019b
wladimirbm-local <wladimirbm@gmail.com>
2025-09-15 07:29:43 +0300

    home2

--- local/app/autoloader.php
+++ local/app/autoloader.php
@@ -2,4 +2,3 @@
 if (file_exists(__DIR__. '/debug/mylog.php')) {
     require_once(__DIR__. '/debug/mylog.php');
 } else die(__DIR__. '/debug/mylog.php');
-?> 
--- local/php_interface/src/autoloader.php
+++ local/php_interface/src/autoloader.php
@@ -2,4 +2,3 @@
 if (file_exists(__DIR__. '/Diag/fileexceptionhandlerlogcustom.php')) {
     require_once(__DIR__. '/Diag/fileexceptionhandlerlogcustom.php');
 } else die(__DIR__. '/Diag/fileexceptionhandlerlogcustom.php');
-?> 
