c247bf7180ab0805058e82e4eafe26239b87a4d6
wladimirbm-local <wladimirbm@gmail.com>
2025-09-15 05:17:39 +0300

    home2

--- otus/homework2/clearlog.php
+++ otus/homework2/clearlog.php
@@ -1,4 +1,4 @@
-<?//require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");?>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");?>
 <?php 
 //use App\Diag\Mylog;
 App\Diag\Mylog::ClearDefLog();
