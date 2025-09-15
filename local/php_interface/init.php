3576367c803905c1a48b3d88dd61437373b22ba0
wladimirbm-local <wladimirbm@gmail.com>
2025-09-15 06:11:05 +0300

    home2

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -4,6 +4,6 @@ if (file_exists(__DIR__ . '/../../vendor/autoload.php')) {
     require_once(__DIR__ . '/../../vendor/autoload.php');
 }
 
-if (file_exists(__DIR__ . '/../app/autoload.php')) {
+//if (file_exists(__DIR__ . '/../app/autoload.php')) {
     require_once(__DIR__ .'/../app/autoloader.php'); 
-}
+//}
--- otus/homework2/clearlog.php
+++ otus/homework2/clearlog.php
@@ -1,4 +1,4 @@
-<?php 
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php"); 
 \App\Diag\Mylog::ClearDefLog();
 //LocalRedirect('/otus/homework2/index.php');
 ?>
