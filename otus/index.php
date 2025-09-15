18e5aafc6f96b421f9fd53aa3eedbe6347ed93d1
wladimirbm-local <wladimirbm@gmail.com>
2025-09-15 08:11:22 +0300

    home2

--- otus/index.php
+++ otus/index.php
@@ -29,14 +29,13 @@ $APPLICATION->SetTitle("Список Домашних работ");
 </style>
 <H1><? $APPLICATION->ShowTitle() ?></H1>
 <p>Репозиторий: <a href="https://github.com/wladimirbm/bitrix24learn">https://github.com/wladimirbm/bitrix24learn</a></p>
-<div>
-    <h2><a href="homework1/">Домашняя работа 1 (Создание и настройка проекта в VScode // ДЗ)</a>
-        <div class="prog done"></div>
-    </h2>
-</div>
-<div>
-    <h2><a href="homework2/">Домашняя работа 2 (Отладка и логирование // ДЗ)</a>
-        <div class="prog proc"></div>
-    </h2>
-</div>
+
+<h2><a href="homework1/">Домашняя работа 1 (Создание и настройка проекта в VScode // ДЗ)</a>
+    <div class="prog done"></div>
+</h2>
+
+<h2><a href="homework2/">Домашняя работа 2 (Отладка и логирование // ДЗ)</a>
+    <div class="prog proc"></div>
+</h2>
+
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
