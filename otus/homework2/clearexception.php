741ca31d759f791ae14fb315e613073ccb867d85
wladimirbm-local <wladimirbm@gmail.com>
2025-09-15 05:21:47 +0300

    home2

--- otus/homework2/clearexception.php
+++ otus/homework2/clearexception.php
@@ -1,5 +1,4 @@
-<?php //require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");?>
 <?php
 App\Diag\Mylog::ClearDefException();
 LocalRedirect('/otus/homework2/index.php');
-?> 
+?>
--- otus/homework2/clearlog.php
+++ otus/homework2/clearlog.php
@@ -1,6 +1,4 @@
-<?//require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");?>
 <?php 
-//use App\Diag\Mylog;
 App\Diag\Mylog::ClearDefLog();
 LocalRedirect('/otus/homework2/index.php');
 ?>
