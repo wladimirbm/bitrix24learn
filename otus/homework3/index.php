4bf4b28435efedbe955394aa9f47f672965aa893
wladimirbm-local <wladimirbm@gmail.com>
2025-09-17 19:43:00 +0300

    home3

--- otus/homework3/index.php
+++ otus/homework3/index.php
@@ -1,6 +1,6 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");?>
 <?php 
-    $APPLICATION->SetTitle("Домашняя работа 2 (Отладка и логирование // ДЗ)");
+    $APPLICATION->SetTitle("Домашняя работа 3 (Связывание моделей // ДЗ)");
 ?>
 <H1><?$APPLICATION->ShowTitle()?></H1>
 <?php
@@ -11,16 +11,10 @@
     echo "</pre>";
     */
 ?>
-<h2>Часть 1 - Logger</h2>
+<h2>Список</h2>
 <p><a href="/logs/mylog_<?php echo date('Y-m-d');?>.log">Файл лога</a></p>
-<p><a href="clearlog.php">Очистить лог</a></p>
-<p><a href="writelog.php">Добавление в лог</a></p>
-<p><a href="/bitrix/admin/fileman_admin.php?lang=ru&site=s1&path=%2Flocal%2Fapp%2Fdebug" target="_blank">Файл с классом кастомного логера</a></p>
 
-<h2>Часть 2 - Exception</h2>
-<p><a href="/logs/exception_<?php echo date('Y-m-d');?>.log">Файл лога</a></p>
-<p><a href="clearexception.php">Очистить лог</a></p>
-<p><a href="writeexception.php">Добавление в лог</a></p>
-<p><a href="/bitrix/admin/fileman_admin.php?PAGEN_1=1&SIZEN_1=20&lang=ru&site=s1&path=%2Flocal%2Fphp_interface%2Fsrc%2FDiag&show_perms_for=0&fu_action=" target="_blank">Файл с классом кастомного логера</a></p>
+<h2>Модель</h2>
+<p><a href="/logs/mylog_<?php echo date('Y-m-d');?>.log">Файл лога</a></p>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?> 
