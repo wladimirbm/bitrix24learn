614ad1fcb543ae09b1e5f509bbad57824d2f519b
wladimirbm-local <wladimirbm@gmail.com>
2025-09-18 02:13:00 +0300

    copy component

--- local/components/bitrix/lists.element.edit/templates/.default/template.php
+++ local/components/bitrix/lists.element.edit/templates/.default/template.php
@@ -462,8 +462,8 @@ $APPLICATION->IncludeComponent(
 			
 			if($arResult["ELEMENT_ID"]==0) { ?>
 				document.querySelector("#tab_el_edit_table > tbody > tr.bx-top > td.bx-field-value > input[type=text]").value="Автоматическое заполнение";
-				document.querySelector("#tab_el_edit_table > tbody > tr.bx-top > td.bx-field-value > input[type=text]").setAttribute('readonly', 'readonly');
 			<?php } ?>
+			document.querySelector("#tab_el_edit_table > tbody > tr.bx-top > td.bx-field-value > input[type=text]").setAttribute('readonly', 'readonly');
 
 			const field1 = document.querySelector("#tab_el_edit_table > tbody > tr:nth-child(2) > td.bx-field-value > input[type=text]");
 			const field2 = document.querySelector("#tab_el_edit_table > tbody > tr:nth-child(3) > td.bx-field-value > input[type=text]");
