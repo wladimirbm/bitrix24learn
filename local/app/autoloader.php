5f5608551e32173abdd014bb6bcb56d9ed930fdd
wladimirbm-local <wladimirbm@gmail.com>
2025-10-10 00:19:38 +0300

    home3

--- local/app/autoloader.php
+++ local/app/autoloader.php
@@ -8,7 +8,7 @@ if (file_exists(__DIR__. '/debug/mylog.php')) {
 spl_autoload_register(function ($className) {
     $classPath = str_replace('\\', '/', $className);
     $file = __DIR__."/$classPath.php";
-    //pr( $file);
+    print_r( $file);
     if (file_exists($file)) {
         include_once $file;
     }
