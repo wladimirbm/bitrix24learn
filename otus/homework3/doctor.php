2c0a2e6c30e8987b2c8e146ea52addd8448f203b
wladimirbm-local <wladimirbm@gmail.com>
2025-10-11 22:18:59 +0300

    home3

--- otus/homework3/doctor.php
+++ otus/homework3/doctor.php
@@ -1,9 +1,12 @@
 <?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php"); ?>
 <?php
+\Bitrix\Main\Loader::includeModule('iblock');
 
+\Bitrix\Main\UI\Extension::load('iblock.field-selector');
 
-if(!empty($_POST)) {
-    dump($_POST); die();
+if (!empty($_POST)) {
+    dump($_POST);
+    die();
 }
 
 
@@ -14,15 +17,16 @@ $docId = 0;
 if (!empty($_GET['docId']) && $_GET['docId'] == (int)$_GET['docId']) {
     $docId = (int)$_GET['docId'];
     $event = "Редактировать";
-} else { $event = "Добавить";
-    $docId=0;
+} else {
+    $event = "Добавить";
+    $docId = 0;
 }
 ?>
 
 <?php
 $APPLICATION->SetTitle($event . " доктора");
 ?>
-<H1><? $APPLICATION->ShowTitle() ?> <?php echo ' <a href="doctors.php">Вернуться к списку</a>';?></H1>
+<H1><? $APPLICATION->ShowTitle() ?> <?php echo ' <a href="doctors.php">Вернуться к списку</a>'; ?></H1>
 
 <?php
 
@@ -64,7 +68,7 @@ else { //if(false)
         $doctor['duty'] = $doctorData->getDuty()->getElement()->getName();
         $doctor['duty_id'] = $doctorData->getDuty()->getElement()->getId();
         $doctor['picture'] = CFile::GetPath($doctorData->getDetailPicture());
-        
+
         foreach ($doctorData->getProcedures()->getAll() as $prItem) {
             //dump($prItem);
             $doctor['procs'][$prItem->getElement()->getId()] = $prItem->getElement()->getName();
@@ -159,8 +163,8 @@ else { //if(false)
                 \Bitrix\Main\UI\Extension::load("ui.inputmask");
                 ?>
                 <input type="text" name="duty" class="date-input" value="<?php echo date('d.m.Y', strtotime($doctor['birthday'])) ?? ''; ?>">
-                   
-                    <script>
+
+                <script>
                     //import {Mask} from 'ui.inputmask';
                     const mask = new BX.UI.Mask({
                         //BX.UI.Inputmask
@@ -168,9 +172,9 @@ else { //if(false)
                         container: document.querySelector('.date-input'),
                         mask: 'xx.xx.xxxx'
                     });
-                    
+
                     mask.init()
-                    </script>
+                </script>
             </td>
         </tr>
         <tr>
@@ -180,6 +184,45 @@ else { //if(false)
             </th>
             <td>
                 <input type="text" name="duty" value="<?php echo $doctor['duty'] ?? ''; ?>">
+                  <?php
+
+
+                //\Bitrix\Main\UI\Extension::load('ui.field-selector');
+
+                $containerId = 'field-duty'; // ID dom-контейнера для TagSelector'а
+
+                if (!empty($doctor['duty']))
+                    $values = array_keys($doctor['duty']); // текущее значение
+                else
+                    $values = [];
+
+                $entities = [[], ['options' => ['enableSearch' => true]]];
+
+                $config = \Bitrix\Main\Web\Json::encode([
+                    'containerId' => $containerId,
+                    'fieldName' => 'duty',
+                    'multiple' => false,
+                    'collectionType' => 'int',
+                    'selectedItems' => $values,
+                    'iblockId' => 17,
+                    'userType' => \Bitrix\Iblock\PropertyTable::USER_TYPE_ELEMENT_AUTOCOMPLETE,
+                    'entityId' => \Bitrix\Iblock\Integration\UI\EntitySelector\IblockPropertyElementProvider::ENTITY_ID,
+                    'entities' => $entities,
+
+                ]);
+
+                echo '
+                            <div id="' . $containerId . '"></div>
+                            <script>
+                                (function() {
+                                    const selector = new BX.Iblock.FieldSelector(' . $config . ');
+                                    //const selector = new BX.UI.FieldSelector(' . $config . ');
+                                    selector.render();
+                                })();
+                            </script>
+                    ';
+
+                ?>
             </td>
         </tr>
         <tr>
@@ -188,47 +231,45 @@ else { //if(false)
                 Процедуры
             </th>
             <td>
-               <?php
+                <?php
 
-                    \Bitrix\Main\Loader::includeModule('iblock');
 
-                    \Bitrix\Main\UI\Extension::load('iblock.field-selector');
-                    //\Bitrix\Main\UI\Extension::load('ui.field-selector');
+                //\Bitrix\Main\UI\Extension::load('ui.field-selector');
 
-                    $containerId = 'field-procedures'; // ID dom-контейнера для TagSelector'а
+                $containerId = 'field-procedures'; // ID dom-контейнера для TagSelector'а
 
-                    if(!empty($doctor['procs']))
-                        $values = array_keys($doctor['procs']); // текущее значение
-                    else
-                        $values = [];
+                if (!empty($doctor['procs']))
+                    $values = array_keys($doctor['procs']); // текущее значение
+                else
+                    $values = [];
 
-                    $entities = [[],['options'=> ['enableSearch' => true]]];
+                $entities = [[], ['options' => ['enableSearch' => true]]];
 
-                    $config = \Bitrix\Main\Web\Json::encode([
-                        'containerId' => $containerId,
-                        'fieldName' => 'procedures',
-                        'multiple' => true,
-                        'collectionType' => 'int',
-                        'selectedItems' => $values,
-                        'iblockId' => 18,
-                        'userType' => \Bitrix\Iblock\PropertyTable::USER_TYPE_ELEMENT_AUTOCOMPLETE,
-                        'entityId' => \Bitrix\Iblock\Integration\UI\EntitySelector\IblockPropertyElementProvider::ENTITY_ID,
-                        'entities' => $entities,
-            
-                    ]);
+                $config = \Bitrix\Main\Web\Json::encode([
+                    'containerId' => $containerId,
+                    'fieldName' => 'procedures',
+                    'multiple' => true,
+                    'collectionType' => 'int',
+                    'selectedItems' => $values,
+                    'iblockId' => 18,
+                    'userType' => \Bitrix\Iblock\PropertyTable::USER_TYPE_ELEMENT_AUTOCOMPLETE,
+                    'entityId' => \Bitrix\Iblock\Integration\UI\EntitySelector\IblockPropertyElementProvider::ENTITY_ID,
+                    'entities' => $entities,
 
-                    echo '
-                            <div id="'.$containerId.'"></div>
+                ]);
+
+                echo '
+                            <div id="' . $containerId . '"></div>
                             <script>
                                 (function() {
-                                    const selector = new BX.Iblock.FieldSelector('.$config.');
-                                    //const selector = new BX.UI.FieldSelector('.$config.');
+                                    const selector = new BX.Iblock.FieldSelector(' . $config . ');
+                                    //const selector = new BX.UI.FieldSelector(' . $config . ');
                                     selector.render();
                                 })();
                             </script>
                     ';
 
-                                                            ?>
+                ?>
             </td>
         </tr>
         <tr>
