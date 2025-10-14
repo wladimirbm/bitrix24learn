88e94e379814bd26fd0f51dd04da1a950dd0be0e
wladimirbm-local <wladimirbm@gmail.com>
2025-10-14 23:09:43 +0300

    home3

--- otus/homework3/doctors.php
+++ otus/homework3/doctors.php
@@ -15,14 +15,14 @@ $APPLICATION->SetAdditionalCSS('/otus/homework3/style.css');
 ?>
 <H1><? $APPLICATION->ShowTitle() ?></H1>
 <?php
-use Models\Lists\DoctorsPropertyValuesTable as DoctorsTable;
-$doctors = DoctorsTable::getList([ // получение списка процедур у врачей
+
+$doctors = \Bitrix\Iblock\Elements\ElementDoctorsTable::getList([ // получение списка процедур у врачей
     'select' => [
         'ID',
         'NAME',
         'DETAIL_PICTURE',
-        'PROCEDURES_NAME',// => 'PROCEDURES.ELEMENT.NAME',
-        'DUTY_NAME',// => 'DUTY.ELEMENT.NAME',
+        'PROCEDURES.ELEMENT.NAME',
+        'DUTY.ELEMENT.NAME',
     ],
     'filter' => [
         //'ID' => $docId,
@@ -33,17 +33,17 @@ $doctors = DoctorsTable::getList([ // получение списка проце
 
 $doctorsList = [];
 foreach ($doctors as $doctor) {
-    $doctorsList[$doctor->getId()]['name'] = $doctor->getName();
+    $doctorsList[$doctor->getId()]['name'] = $doctor->getName() ?? '';
     echo $doctor->getId()."<br>";
-    $doctorsList[$doctor->getId()]['duty'] = $doctor->getDutyName()->getElement()->getName();
+    $doctorsList[$doctor->getId()]['duty'] = $doctor->getDuty()->getElement()->getName() ?? '';
     // dump($doctor->getId() . ' ' . $doctor->getName() . ' - - -');
     // dump(CFile::GetPath($doctor->getDetailPicture()));
     // dump($doctor->getDuty()->getElement()->getName());
 
-    foreach ($doctor->getProceduresName()->getAll() as $prItem) {
+    foreach ($doctor->getProcedures()->getAll() as $prItem) {
         // получаем значение свойства Описание у процедуры 
         //if($prItem->getElement()->getDescription()!== null){
-        $doctorsList[$doctor->getId()]['proc'][$prItem->getId()] = $prItem->getElement()->getName();
+        $doctorsList[$doctor->getId()]['proc'][$prItem->getId()] = $prItem->getElement()->getName() ?? '';
         // dump($prItem->getId() . ' - ' . $prItem->getElement()->getName()/*.' - '.$prItem->getElement()->getDescription()->getValue() */);
         //}
         // получаем значение свойства Цвет у процедуры 
