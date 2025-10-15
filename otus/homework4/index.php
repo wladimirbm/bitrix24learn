45f6b518f9316df94d6cd77b73990669b0c3aa7c
wladimirbm-local <wladimirbm@gmail.com>
2025-10-15 23:47:09 +0300

    home4

--- otus/homework4/index.php
+++ otus/homework4/index.php
@@ -11,11 +11,13 @@
     echo "</pre>";
     */
 ?>
-<h2>Работа со моделями</h2>
-<p><a href="doctors.php">Список докторов</a></p>
+<h2>Создание своих таблиц БД и написание модели данных к ним</h2>
+<p><a href="tables.php">Таблицы</a></p>
 
 <h2>Файлы</h2>
+<!-- 
 <p><a href="http://cc61466.tw1.ru/bitrix/admin/fileman_admin.php?PAGEN_1=1&SIZEN_1=20&lang=ru&site=s1&path=%2Flocal%2Fapp%2FModels&show_perms_for=0&fu_action=">Файлы модели</a></p>
 <p><a href="http://cc61466.tw1.ru/bitrix/admin/fileman_admin.php?lang=ru&path=%2Fotus%2Fhomework3&site=s1">Файлы ДЗ</a></p>
+-->
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?> 
