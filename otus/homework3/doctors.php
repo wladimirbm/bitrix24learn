6ca48aec21712c8e4e57ce59cfeaaabd4aec02cd
wladimirbm-local <wladimirbm@gmail.com>
2025-10-15 16:12:03 +0300

    home3

--- otus/homework3/doctors.php
+++ otus/homework3/doctors.php
@@ -41,6 +41,7 @@ foreach ($doctors as $doctor) {
     //echo $doctor->getId()."<br>";
     
     $doctorsList[$doctor->getId()]['duty'] = ($doctor->getDutyId()->getElement()->getName()!==null) ? $doctor->getDutyId()->getElement()->getName() : ''; 
+    $doctorsList[$doctor->getId()]['duty'] = ($doctor->getDutyName()->getValue()!==null) ? $doctor->getDutyName()->getValue() : ''; 
     //->getElement()->getName() ?? ''; //->getElement()->getName() ?? '';
 
     // dump($doctor->getId() . ' ' . $doctor->getName() . ' - - -');
