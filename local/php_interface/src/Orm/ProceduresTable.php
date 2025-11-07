fdc396b14a7ff26d8f2f6383855dc1c19dea3afe
wladimirbm-local <wladimirbm@gmail.com>
2025-11-08 02:37:16 +0300

    home4

--- local/php_interface/src/Orm/ProceduresTable.php
+++ local/php_interface/src/Orm/ProceduresTable.php
@@ -77,9 +77,9 @@ class ProceduresTable extends DataManager
 			))->configureTitle(Loc::getMessage('ELEMENT_PROP_S18_ENTITY_IBLOCK_ELEMENT_ID_FIELD'))
 				->configurePrimary(true),
 
-			(new Fields\StringField('NAME'))
-                ->configureRequired(true)
-                ->configureTitle('Название процедуры'),
+			// (new Fields\StringField('NAME'))
+            //     ->configureRequired(true)
+            //     ->configureTitle('Название процедуры'),
 			// Связь со свойствами инфоблока
             //'PROPERTIES' => (new Fields\Relations\OneToMany('PROPERTIES', ProceduresPropertyTable::class, 'ELEMENT')),
 				
