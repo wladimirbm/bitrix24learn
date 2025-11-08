35b18545f9368f78c7bc0f809b2bfeebe55ef945
wladimirbm-local <wladimirbm@gmail.com>
2025-11-08 05:46:31 +0300

    home4

--- otus/homework4/index.php
+++ otus/homework4/index.php
@@ -1,6 +1,6 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");?>
 <?php 
-    $APPLICATION->SetTitle("Домашняя работа 3 (Связывание моделей // ДЗ)");
+    $APPLICATION->SetTitle("Домашняя работа 4 (Создание своих таблиц БД и написание модели данных к ним // ДЗ)");
 ?>
 <H1><?$APPLICATION->ShowTitle()?></H1>
 <?php
@@ -21,6 +21,6 @@
 <p><a href="/bitrix/admin/fileman_admin.php?PAGEN_1=1&SIZEN_1=20&lang=ru&site=s1&path=%2Flocal%2Fphp_interface%2Fconsole&show_perms_for=0&fu_action=">Файлы создания и удаления таблиц</a></p>
 <p><a href="/bitrix/admin/fileman_admin.php?lang=ru&path=%2Fotus%2Fhomework4&site=s1">Файл ДЗ</a></p>
 
-
+ 
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?> 
