3d0419f0e0c97a5a1e580a7a739838eeea1ec204
wladimirbm-local <wladimirbm@gmail.com>
2025-11-23 18:51:24 +0300

    home6

--- otus/homework6/index.php
+++ otus/homework6/index.php
@@ -12,12 +12,18 @@
     */
 ?>
 <h2>Написание своего модуля</h2>
-<p><a href="currencies.php">Мудуль</a></p>
+<p><a href="/crm/deal/list/">Сделки</a></p>
+<p><a href="/crm/lead/list/">Лиды</a></p>
+<p><a href="/crm/contact/list/">Контакты</a></p>
+<p><a href="/crm/company/list/">Компании</a></p>
+
 
 <h2>Файлы</h2>
- 
 
-<p><a href="/bitrix/admin/fileman_admin.php?PAGEN_1=1&SIZEN_1=20&lang=ru&site=s1&path=%2Flocal%2Fcomponents%2Fotus%2Fshowcurrency&show_perms_for=0&fu_action=">Файлы компонента</a></p>
- 
+<p><a href="/bitrix/admin/partner_modules.php?lang=ru">Установка/Удаление</a></p>
+<p><a href="/bitrix/admin/settings.php?mid=otus.crmcustomtab&lang=ru">Настройки</a></p>
+<p><a href="/bitrix/admin/perfmon_tables.php?lang=ru#mod_">Таблицы</a></p>
+<p><a href="/bitrix/admin/fileman_admin.php?lang=ru&path=%2Flocal%2Fmodules%2Fotus.crmcustomtab&site=s1">Файлы модуля</a></p>
+
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?> 
