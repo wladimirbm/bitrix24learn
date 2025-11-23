aaf3dfd6449f2c00c6cf83ce37450fe015d45173
wladimirbm-local <wladimirbm@gmail.com>
2025-11-23 20:06:29 +0300

    home6

--- otus/homework6/index.php
+++ otus/homework6/index.php
@@ -12,10 +12,10 @@
 <p><a href="/crm/deal/list/">Сделки</a></p>
 <p><a href="/crm/lead/list/">Лиды</a></p>
 
-<h2>Файлы</h2>
+<h2>Ссылки</h2>
 
-<p><a href="/bitrix/admin/partner_modules.php?lang=ru">Установка/Удаление</a></p>
-<p><a href="/bitrix/admin/settings.php?mid=otus.crmcustomtab&lang=ru">Настройки</a></p>
+<p><a href="/bitrix/admin/partner_modules.php?lang=ru">Установка/Удаление модуля</a></p>
+<p><a href="/bitrix/admin/settings.php?mid=otus.crmcustomtab&lang=ru">Настройки модуля</a></p>
 <p><a href="/bitrix/admin/perfmon_tables.php?lang=ru#mod_">Таблицы</a></p>
 <p><a href="/bitrix/admin/fileman_admin.php?lang=ru&path=%2Flocal%2Fmodules%2Fotus.crmcustomtab&site=s1">Файлы модуля</a></p>
 
