fd9981e8da960d3e2a31a45341ad91c9c36b9719
wladimirbm-local <wladimirbm@gmail.com>
2025-11-26 01:03:21 +0300

    home8

---
+++ local/php_interface/events.php
@@ -0,0 +1,57 @@
+<?php
+
+//use \Bitrix\Main\EventManager;
+\Bitrix\Main\EventManager::getInstance()->addEventHandler(
+    'main',
+    'OnProlog',
+    [CustomEvents::class, 'OnProlog']
+);
+
+\Bitrix\Main\EventManager::getInstance()->AddEventHandler(
+    "main",
+    "OnBeforeProlog",
+    [CustomEvents::class, "OnBeforePrologHandler"]
+);
+
+class CustomEvents
+{
+    public static function OnProlog()
+    {
+        global $USER;
+        $arJsConfig = array(
+            'custom_start' => array(
+                'js' => '/local/additional/main.js',
+                'css' => '/local/additional/main.css',
+                'rel' => array()
+            )
+        );
+        foreach ($arJsConfig as $ext => $arExt) {
+            \CJSCore::RegisterExt($ext, $arExt);
+        }
+        CUtil::InitJSCore(array('custom_start'));
+
+        //CJSCore::Init(array('jquery', 'ajax', 'popup'));
+
+        $asset = \Bitrix\Main\Page\Asset::getInstance();
+
+        $settings = [];
+
+        // if (preg_match('/\/crm.*/', GetPagePath())) {
+        //    $asset->addString('<script>BX.ready(function () { Dreamsite.crm(' . CUtil::PhpToJSObject($settings) . '); });</script>');
+        // }
+
+        if (preg_match('/\/crm\/company\/details\/.*/', GetPagePath())) {
+            //$asset->addString('<link rel="stylesheet" type="text/css" href="/local/js/dreamsite/datatables/datatables.min.css"/><script type="text/javascript" src="/local/js/dreamsite/datatables/datatables.min.js"></script>');
+            // $asset->addString('<script>BX.ready(function () { Custom.crmCompany(); });</script>');
+        }
+
+        //На всех страницах
+        $asset->addString('<script>BX.ready(function () { Dreamsite.all(); });</script>');
+    }
+
+
+    public static function OnBeforePrologHandler()
+    {
+        CJSCore::Init(array('jquery2'));
+    }
+}
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -8,67 +8,8 @@ if (file_exists(__DIR__ . '/../app/autoloader.php')) {
     require_once(__DIR__ . '/../app/autoloader.php');
 }
 
-// if (file_exists(__DIR__ . '/src/autoloader.php')) {
-//     require_once(__DIR__ .'/src/autoloader.php'); 
-// }
-?>
-
-<?php
-
-// use \Bitrix\Main\EventManager;
-// EventManager::getInstance()->addEventHandler(
-//     'main',
-//     'OnProlog',
-//     [CustomEvents::class, 'OnProlog']
-// );
-
-// EventManager::getInstance()->AddEventHandler(
-//     "main",
-//     "OnBeforeProlog",
-//     [CustomEvents::class, "OnBeforePrologHandler"]
-// );
-
-class CustomEvents
-{
-    public static function OnProlog()
-    {
-        global $USER;
-        $arJsConfig = array(
-            'custom_start' => array(
-                'js' => '/local/additional/main.js',
-                'css' => '/local/additional/main.css',
-                'rel' => array()
-            )
-        );
-        foreach ($arJsConfig as $ext => $arExt) {
-            \CJSCore::RegisterExt($ext, $arExt);
-        }
-        CUtil::InitJSCore(array('custom_start'));
-
-        //CJSCore::Init(array('jquery', 'ajax', 'popup'));
-
-        $asset = \Bitrix\Main\Page\Asset::getInstance();
-
-        $settings = [];
-
-        // if (preg_match('/\/crm.*/', GetPagePath())) {
-        //    $asset->addString('<script>BX.ready(function () { Dreamsite.crm(' . CUtil::PhpToJSObject($settings) . '); });</script>');
-        // }
-
-        if (preg_match('/\/crm\/company\/details\/.*/', GetPagePath())) {
-            //$asset->addString('<link rel="stylesheet" type="text/css" href="/local/js/dreamsite/datatables/datatables.min.css"/><script type="text/javascript" src="/local/js/dreamsite/datatables/datatables.min.js"></script>');
-            // $asset->addString('<script>BX.ready(function () { Custom.crmCompany(); });</script>');
-        }
-
-        //На всех страницах
-        $asset->addString('<script>BX.ready(function () { Dreamsite.all(); });</script>');
-    }
-
-
-    public static function OnBeforePrologHandler()
-    {
-        CJSCore::Init(array('jquery2'));
-    }
+if (file_exists(__DIR__ . '/events.php')) {
+    require_once(__DIR__ .'/events.php'); 
 }
 
-?>
+
