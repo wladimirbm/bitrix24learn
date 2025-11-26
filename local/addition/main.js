9e930cbdac05be4fd0c9b1946d91cb364c1d476a
wladimirbm-local <wladimirbm@gmail.com>
2025-11-26 05:06:07 +0300

    home8

--- local/addition/main.js
+++ local/addition/main.js
@@ -72,6 +72,7 @@ BX.addCustomEvent("onAjaxSuccessFinish", function(result) {
         console.log('action', action);
        
         if (action === 'start' || action === 'reopen') {
+            request.cancel = true;
             bitrixConfirm(action).then((confirmed) => {
                 if (!confirmed) {
                     console.log('Действие отменено:', action);
