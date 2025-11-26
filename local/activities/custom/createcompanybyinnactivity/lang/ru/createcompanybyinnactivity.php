74488757e2dd7847afcdbc1e77a1f3c7d74e036f
wladimirbm-local <wladimirbm@gmail.com>
2025-11-27 02:36:28 +0300

    home9

--- local/activities/custom/createcompanybyinnactivity/lang/ru/createcompanybyinnactivity.php
+++ local/activities/custom/createcompanybyinnactivity/lang/ru/createcompanybyinnactivity.php
@@ -1,2 +1,2 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
-$MESS['CCBYINN_ACTIVITY_FIELD_SUBJECT']  = 'Инн';
+$MESS['CCBYINN_ACTIVITY_FIELD_SUBJECT']  = 'Выберите поле с Инн';
