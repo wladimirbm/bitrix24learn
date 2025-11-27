3940b6661078d4eedfc1b3dd37c298a5f3788b06
wladimirbm-local <wladimirbm@gmail.com>
2025-11-27 23:33:12 +0300

    home8

--- local/js/otus/confirm_workday/config.php
+++ local/js/otus/confirm_workday/config.php
@@ -1,8 +1,8 @@
-<?php 
+<?
 if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 
 return [
-    'js' => './main.js',
+    'js' => './main.js',  
     'rel' => ['ui.popup', 'main.core'],
     'skip_core' => false,
 ];
