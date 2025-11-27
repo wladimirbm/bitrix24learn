9f77f8124d9a2a06c3fe12b291c9b1713aacb1a9
wladimirbm-local <wladimirbm@gmail.com>
2025-11-27 04:36:40 +0300

    home9

--- otus/homework9/index.php
+++ otus/homework9/index.php
@@ -14,7 +14,7 @@ $APPLICATION->SetTitle("Домашняя работа 9 (Написание св
  
 
 <?php
-/*
+
 $token = "261c33510bdc7d1bb98565cfd1e37425b2ee3b2a";
 $secret = "832a12ffd03501b87df857c63b3eda10c205e44a";
 
@@ -22,10 +22,10 @@ $dadata = new Dadata($token, $secret);
 $dadata->init();
 
 // Найти компанию по ИНН
-$fields = array("query" => "7707083893", "count" => 5);
+$fields = array("query" => "7707083893", "count" => 1);
 $result = $dadata->suggest("party", $fields);
 dump($result);
-*/
+
 ?>
 
 
