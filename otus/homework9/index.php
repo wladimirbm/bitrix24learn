cac8d3c45356b7394b14d3df425e80f9e83a245d
wladimirbm-local <wladimirbm@gmail.com>
2025-11-27 04:21:04 +0300

    home9

--- otus/homework9/index.php
+++ otus/homework9/index.php
@@ -6,12 +6,15 @@ $APPLICATION->SetTitle("Домашняя работа 9 (Написание св
 
 <h2>Написание своих активити для БП</h2>
 
-<p><a href="/crm/">CRM</a></p>
+<p><a href="/services/lists/19/view/0/">Инфоблок</a></p>
 
 <h2>Файлы</h2>
-<p><a href="/bitrix/admin/perfmon_tables.php?lang=ru#mod_">Таблицы</a></p>
+<p><a href="/bitrix/admin/fileman_admin.php?PAGEN_1=1&SIZEN_1=20&lang=ru&site=s1&path=%2Flocal%2Factivities%2Fcustom%2Fcreatecompanybyinnactivity&show_perms_for=0&fu_action=">Файлы активити</a></p>
+<p><a href="/services/lists/19/bp_edit/6/">Бизнес процесс</a></p>
+
 
 <?php
+/*
 $token = "261c33510bdc7d1bb98565cfd1e37425b2ee3b2a";
 $secret = "832a12ffd03501b87df857c63b3eda10c205e44a";
 
@@ -22,6 +25,7 @@ $dadata->init();
 $fields = array("query" => "7707083893", "count" => 5);
 $result = $dadata->suggest("party", $fields);
 dump($result);
+*/
 ?>
 
 
