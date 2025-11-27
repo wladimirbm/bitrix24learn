1a2b5de5dc185305771693acbf859523b18948e2
wladimirbm-local <wladimirbm@gmail.com>
2025-11-27 04:11:58 +0300

    home9

--- local/activities/custom/createcompanybyinnactivity/createcompanybyinnactivity.php
+++ local/activities/custom/createcompanybyinnactivity/createcompanybyinnactivity.php
@@ -105,19 +105,21 @@ class CBPCreateCompanyByInnactivity extends BaseActivity
 
             $fields = [
                 'NAME' => $response['suggestions'][0]['value'],
-                'CUSTOMER' => ["COMPANY_" . $companyID],
+                'PROPERTY_CUSTOMER' => $companyID,
             ];
 
             $resultFields = $this->prepareFieldsValues($documentId, $documentType, $fields);
             $documentService->UpdateDocument($documentId, $resultFields);
 
-
+            /*
             $documentFields =  $documentService->GetDocumentFields($documentType);
             //$arDocumentFields = $documentService->GetDocument($documentId);   
-
+          
             foreach ($documentFields as $key => $value) {
                 $this->log($key . ' => ' . $value);
             }
+            */
+
             /*
             $iblockElement = new CIBlockElement();
 
