ef813aefb32a070efec8325866f1506e8ce18927
wladimirbm-local <wladimirbm@gmail.com>
2025-12-25 22:34:42 +0300

    home11

--- local/app/webhook/oncrmactivityadd.php
+++ local/app/webhook/oncrmactivityadd.php
@@ -6,3 +6,30 @@
 //echo "hello";
 file_put_contents('../../../logs/webhook.log', print_r($_REQUEST, true) . PHP_EOL, FILE_APPEND);
 //https://cc61466.tw1.ru/rest/1/kuzuzl8fna81k2n0/
+
+require_once(__DIR__ . '/crest.php');
+if (!empty($_REQUEST['data']['FIELDS']['ID']))
+    $resultActivity = CRest::call(
+        'crm.activity.get',
+        [
+            'id' => $_REQUEST['data']['FIELDS']['ID']
+        ]
+    );
+
+if (!empty($resultActivity['result'])) {
+    if ($resultActivity['result']['OWNER_TYPE_ID'] == 3)
+
+        $result = CRest::call(
+            'crm.contact.update',
+            [
+                'ID' => $resultActivity['result']['OWNER_ID'],
+                'FIELDS' => [
+                    'UF_CRM_1766686468932' => date('Y-m-d', strtotime($resultActivity['result']['CREATED'])),
+                ],
+                'PARAMS' => [
+                    'REGISTER_SONET_EVENT' => 'N',
+                    'REGISTER_HISTORY_EVENT' => 'N',
+                ]
+            ]
+        );
+}
