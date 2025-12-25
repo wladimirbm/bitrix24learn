165ea95434f60a5c7620917f12e2be07f610f8bd
wladimirbm-local <wladimirbm@gmail.com>
2025-12-25 21:53:30 +0300

    home11

--- local/app/webhook/oncrmactivityadd.php
+++ local/app/webhook/oncrmactivityadd.php
@@ -3,5 +3,5 @@
 
 // use App\Debug\Mylog;
 // Mylog::addLog($_REQUEST, '$_REQUEST', '', __FILE__, __LINE__);
-echo "hello";
-file_put_contents('webhook.log', print_r($_REQUEST, true) . PHP_EOL, FILE_APPEND);
+//echo "hello";
+file_put_contents($_SERVER['DOCUMENT_ROOT'].'/logs/webhook.log', print_r($_REQUEST, true) . PHP_EOL, FILE_APPEND);
