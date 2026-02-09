602add9f293b7940122ee3c7df6d3329e7986c7b
wladimirbm-local <wladimirbm@gmail.com>
2026-02-09 19:27:00 +0300

    project

--- local/php_interface/agents/smarts.php
+++ local/php_interface/agents/smarts.php
@@ -102,7 +102,7 @@ class Agents
         $context = new \Bitrix\Crm\Service\Context();
         $context->setUserId(13);
 
-        $operation = $factory->getAddOperation($itemб $context);
+        $operation = $factory->getAddOperation($item, $context);
         $result = $operation->launch();
 
         if (!$result->isSuccess()) {
