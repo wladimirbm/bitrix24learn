994df581707e6644785cfdbdb0ef6a4cfb8ab472
wladimirbm-local <wladimirbm@gmail.com>
2026-02-09 10:46:29 +0300

    project

--- otus/project/index.php
+++ otus/project/index.php
@@ -17,6 +17,7 @@ $APPLICATION->SetTitle('Проектная работа "Внедрение Би
 <p><a href="/crm/catalog/list/16/?IBLOCK_ID=14&type=CRM_PRODUCT_CATALOG&lang=ru&find_section_section=16&SECTION_ID=16&apply_filter=Y">Запчасти</a></p>
 <h3>Агент</h3>
 <p><a href="/bitrix/admin/fileman_admin.php?lang=ru&path=%2Flocal%2Fphp_interface%2Fagents&site=s1">Код</a></p>
+<p><a href="/bitrix/admin/agent_edit.php?ID=865&editTab_active_tab=tab1&lang=ru">Настройка</a></p>
 <p><a href="/bitrix/admin/perfmon_table.php?lang=ru&table_name=b_agent&apply_filter=Y&by=ID&order=desc">Таблица</a></p>
 <h3>Роботы</h3>
 <p><a href="/page/zakupshchik/zayavka_na_zakupku/type/1058/automation/11/">Заявка на покупку</a></p>
