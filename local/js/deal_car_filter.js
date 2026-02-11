8b778f1621b75c2ae200e3c3588893d0b9777c7c
wladimirbm-local <wladimirbm@gmail.com>
2026-02-11 03:54:59 +0300

    project

--- local/js/deal_car_filter.js
+++ local/js/deal_car_filter.js
@@ -121,6 +121,7 @@
     formData.append("FILTER[DYNAMICS_1054][=CONTACT_ID]", contactId);
     formData.append("entityTypes[DYNAMICS_1054][options][filter][CONTACT_ID]", contactId);
     formData.append("entityTypes[DYNAMICS_1054][options][CONTACT_ID]", contactId);
+    formData.append("entityTypes[DYNAMICS_1054][options][ParentID ]", contactId);
 
     console.log("Запрос с контактом:", contactId);
 
